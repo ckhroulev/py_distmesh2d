9d21fc1bae88f2a325e0d0f3e34887bc19f9fc10
Constantine Khroulev <ckhroulev@alaska.edu>
2012-03-25 21:33:45 -0800

    Still works.

--- py_distmesh2.py
+++ py_distmesh2.py
@@ -3,15 +3,22 @@
 from scipy.spatial import Delaunay
 import numpy as np
 from numpy import sqrt, sum
-from pylab import figure, triplot, axes, show, clf, vstack
+from pylab import figure, triplot, axis, axes, show, clf, vstack
 
 def dcircle(pts, *args):
     return sqrt(pts[:,0]**2 + pts[:,1]**2) - 1.0
 
-def fd(pts, *args):
+def drectangle(pts, x1, x2, y1, y2):
+    return -np.min(np.min(np.min(-y1+p[:,1], y2-p[:,1]),
+                          -x1+p[:,0], x2-p[:,0]))
+
+def example1(pts, *args):
+    return sqrt(sum(pts**2, 1)) - 1
+
+def example2(pts, *args):
     return -0.3 + np.abs(0.7 - sqrt(sum(pts**2, 1)))
 
-def fh(pts, *args):
+def huniform(pts, *args):
     return np.ones((pts.shape[0], 1))
 
 def distmesh2d(fd, fh, h0, bbox, pfix, *args):
@@ -44,6 +51,7 @@ def distmesh2d(fd, fh, h0, bbox, pfix, *args):
             t = t[fd(pmid, *args) < -geps]
             bars = t[:, [[0,1], [1,2], [0,2]]].reshape((-1, 2))
             bars = np.unique(bars.view("i,i")).view("i").reshape((-1,2))
+            print ":"
 
         barvec = p[bars[:,0]] - p[bars[:,1]]
         L = sqrt(sum(barvec**2, 1)).reshape((-1,1))
@@ -65,6 +73,7 @@ def distmesh2d(fd, fh, h0, bbox, pfix, *args):
 
         p[ix] -= vstack((d[ix] * dgradx, d[ix] * dgrady)).T
 
+        print "."
         if (sqrt(sum((deltat * Ftot[d < -geps])**2, 1)) / h0).max() < dptol:
             break
 
@@ -72,11 +81,14 @@ def distmesh2d(fd, fh, h0, bbox, pfix, *args):
 
 figure()
 
-h0 = 0.15
-pts, tri = distmesh2d(fd, fh, h0, [[-1, 1], [-1, 1]], [])
+h0 = 0.1
+bbox = [[-1, 1], [-1, 1]]
+pfix = [[-1,-1], [-1,1], [1,-1], [1,1]]
+pts, tri = distmesh2d(example1, huniform, h0, bbox, [])
 
 triplot(pts[:,0], pts[:,1], tri, "b-", lw=2)
 
+axis('tight')
 axes().set_aspect('equal')
 
 show()
