443f9ed03e39cf6716334eb53f852dc8fcde78b4
Constantine Khroulev <ckhroulev@alaska.edu>
2012-03-26 07:56:57 -0800

    README, a figure, some comments...

--- py_distmesh2.py
+++ py_distmesh2.py
@@ -2,8 +2,8 @@
 
 from scipy.spatial import Delaunay
 import numpy as np
-from numpy import sqrt, sum
-from pylab import figure, triplot, axis, axes, show, clf, vstack
+from numpy import sqrt, sum, vstack
+from pylab import figure, triplot, tripcolor, axis, axes, show
 
 def dcircle(pts, xc, yc, r):
     return sqrt((pts[:,0] - xc)**2 + (pts[:,1] - yc)**2) - r
@@ -37,10 +37,11 @@ def huniform(pts, *args):
     return np.ones((pts.shape[0], 1))
 
 def distmesh2d(fd, fh, h0, bbox, pfix, *args):
-    global p, t, F, barvec, L, L0, hbars
+    # parameters
     dptol = 0.001; ttol = 0.1; Fscale = 1.2; deltat = 0.2;
     geps = 0.001 * h0; deps = sqrt(np.finfo(float).eps) * h0
 
+    # create the initial point distribution:
     bbox = np.array(bbox)
     x, y = np.meshgrid(np.arange(bbox[0,0], bbox[0,1], h0),
                        np.arange(bbox[1,0], bbox[1,1], h0 * sqrt(3) / 2))
@@ -53,21 +54,29 @@ def distmesh2d(fd, fh, h0, bbox, pfix, *args):
     r0 = 1.0 / fh(p, *args)**2
     selection = np.random.rand(p.shape[0], 1) < r0 / r0.max()
     p = p[selection[:,0]]
+
+    # add fixed points:
     if len(pfix) > 0:
         p = np.vstack((pfix, p))
 
-    pold = np.zeros_like(p)
+    pold = np.zeros_like(p); pold[:] = np.inf
     Ftot = np.zeros_like(p)
 
     def triangulate(pts):
+        """
+        Compute the Delaunay triangulation and remove trianges with
+        centroids outside the domain.
+        """
         tri = np.sort(Delaunay(pts).vertices, axis=1)
         pmid = sum(pts[tri], 1) / 3
         return tri[fd(pmid, *args) < -geps]
 
     while True:
+        # check if it is time to re-compute the triangulation
         if sqrt(sum((p - pold)**2, 1)).max() > ttol:
             pold[:] = p[:]
             t = triangulate(p)
+            # find unique edges of trianges
             bars = t[:, [[0,1], [1,2], [0,2]]].reshape((-1, 2))
             bars = np.unique(bars.view("i,i")).view("i").reshape((-1,2))
 
@@ -79,27 +88,34 @@ def distmesh2d(fd, fh, h0, bbox, pfix, *args):
         F = np.maximum(L0 - L, 0)
         Fvec = F * (barvec / L)
 
+        # compute total forces for each point:
         Ftot[:] = 0
         for j in xrange(bars.shape[0]):
             Ftot[bars[j]] += [Fvec[j], -Fvec[j]]
 
+        # zero out forces at fixed points:
         Ftot[0:len(pfix), :] = 0.0
 
+        # update point locations:
         p += deltat * Ftot
 
+        # find points that ended up outside the domain and move them to the boundary:
         d = fd(p, *args); ix = d > 0
         dgradx = (fd(vstack((p[ix,0] + deps, p[ix,1])).T, *args)        - d[ix]) / deps
         dgrady = (fd(vstack((p[ix,0],        p[ix,1] + deps)).T, *args) - d[ix]) / deps
-
         p[ix] -= vstack((d[ix] * dgradx, d[ix] * dgrady)).T
 
+        # stopping criterion:
         if (sqrt(sum((deltat * Ftot[d < -geps])**2, 1)) / h0).max() < dptol:
             break
 
     return p, triangulate(p)
 
-def plot_mesh(pts, tri):
-    triplot(pts[:,0], pts[:,1], tri, "b-", lw=2)
+def plot_mesh(pts, tri, *args):
+    if len(args) > 0:
+        tripcolor(pts[:,0], pts[:,1], tri, args[0], edgecolor='black', cmap="Blues")
+    else:
+        triplot(pts[:,0], pts[:,1], tri, "k-", lw=2)
     axis('tight')
     axes().set_aspect('equal')
 
@@ -128,6 +144,11 @@ plot_mesh(pts, tri)
 # example 3a
 figure()
 pts, tri = distmesh2d(example3, huniform, 0.15, bbox, square)
+plot_mesh(pts, tri, example3(pts))
+
+# example 3b
+figure()
+pts, tri = distmesh2d(example3, example3_h, 0.035, bbox, square)
 plot_mesh(pts, tri)
 
 show()
